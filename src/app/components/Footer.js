92cc770ee6d71a0e06da012c799fbbd7c15719d1
Chirag <121595688+imchiraghere@users.noreply.github.com>
2023-07-06 16:56:44 +0530

    Update Footer.js

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "@/app/Styles/footer.module.css";
+import styles from "@/app/styles/footer.module.css";
 
 export default function Footer() {
   return (
